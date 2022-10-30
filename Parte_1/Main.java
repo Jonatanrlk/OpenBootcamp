1712d0c7478d4d3be6043e037d1f6a23d24fe503
JonatanR <89276258+Jonatanrlk@users.noreply.github.com>
2022-10-30 13:35:54 -0500

    Parte 1 Terminada

---
+++ Parte_1/Main.java
@@ -0,0 +1,14 @@
+package Parte_1;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        suma(20 , 50 , 30);
+    }
+
+    public static void suma(int a , int b, int c)
+    {
+        System.out.println("La suma es :"+(a+b+c));
+    }
+}
